c20eb7d13e67dbec565fc0e8fe96a2b81801091c
USEINOV RASIM <r.usieinov@gmail.com>
2023-02-28 14:18:08 +0300

    refactoring

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
-RSpec.describe QuestionsController, type: controller do
-  describe
+RSpec.describe QuestionsController, type: :controller do
+  describe 'GET #index' do
+    it 'populates and array of all questions' do
+      question1 = FactoryBot.create(:question)
+      question2 = FactoryBot.create(:question)
+
+      get :index
+
+      expect(aasigns(:questions)).to match_array([question1, question2])
+    end
+
+    it 'render index view ' do
+      get :index
+      expect(responce).to render_template :index
+    end
+  end
 end
+
+
