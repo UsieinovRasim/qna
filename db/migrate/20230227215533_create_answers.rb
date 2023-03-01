4e24d98f6b0752d05721315eea89748b46df5965
USEINOV RASIM <r.usieinov@gmail.com>
2023-03-01 15:42:23 +0300

    revert version

--- db/migrate/20230227215533_create_answers.rb
+++ db/migrate/20230227215533_create_answers.rb
@@ -1,4 +1,4 @@
-class CreateAnswers < ActiveRecord::Migration[5.2]
+class CreateAnswers < ActiveRecord::Migration[7.0]
   def change
     create_table :answers do |t|
       t.text :body, null: false
