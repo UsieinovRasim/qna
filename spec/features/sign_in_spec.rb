676c8e2392bf55db0ebcd561fc418764be847973
USEINOV RASIM <r.usieinov@gmail.com>
2023-03-02 01:07:42 +0300

    rebase

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -9,7 +9,7 @@ feature 'User can sign in', %q{
   given(:user) { User.create!(email: 'user@test.com', password: '12345678') }
 
   background { visit new_user_session_path }
-  
+
   scenario 'Registered user tries to sign in' do
     fill_in 'Email', with: user.email
     fill_in 'Password', with: user.password
